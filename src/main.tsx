8def53297521958c3fce7eb71caf87ca4ae66980
Skill Template <template@skill>
2026-06-28 06:01:14 +0800

    chore(backend-building): graft features db

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import { AuthProvider } from '@/context/AuthContext'
 import './index.css'
+import { TRPCProvider } from "@/providers/trpc"
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <TRPCProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </TRPCProvider>
     </BrowserRouter>
   </StrictMode>,
 )
